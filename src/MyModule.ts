c6fc1bb10edb41b72dd4ebb8416153ae91ae2641
SunKyu Choi <98688494+luke0408@users.noreply.github.com>
2025-03-23 06:09:15 +0000

    refactor: remove unrelated code

--- src/MyModule.ts
+++ src/MyModule.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 
-import { BbsArticleModule } from "./controllers/bbs/BbsArticleModule";
 import { ChatModule } from "./controllers/notion/NotionModule";
 
 @Module({
@@ -11,7 +10,6 @@ import { ChatModule } from "./controllers/notion/NotionModule";
       rootPath: join(__dirname, "..", "client"),
       serveRoot: "/",
     }),
-    BbsArticleModule,
     ChatModule,
   ],
 })
